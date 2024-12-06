b1c27114e2652ced21493c236d648c3acb4204a3
Luiza Caldeira <luiza.caldeira@estudante.ufjf.br>
2024-12-05 22:12:27 -0300

    Adiciona filhos da Exibição.

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,28 @@ import Exibicao from './Exibicao';
 import './App.css';
 
 function App() {
-  return <div className='app'>
-    <Exibicao abertura={new Date("2024-11-06T08:00:00.000-03:00")}fechamento={new Date("2024-11-06T12:00-03:00")} cercado="A1"/>
-    <Animal icone="🦁" nome="Leão" peso={-1} extincao={true} />
-    <Animal icone="🦒" nome="Girafa" peso={1200} extincao={true} />
-    <Exibicao abertura={new Date("2024-11-06T13:00:00.000-03:00")}fechamento={new Date("2024-11-06T17:00-03:00")} cercado="B1"/>
-    <Animal icone="🦜" nome="Papagaio" peso={1.2} extincao={true} />
+  return (
+  <div className='app'>
+    <Exibicao 
+    abertura={new Date("2024-11-06T08:00:00.000-03:00")}
+    fechamento={new Date("2024-11-06T12:00-03:00")}
+    cercado="A1"
+    >
+        <Animal icone="🦁" nome="Leão" peso={-1} extincao={true} />
+        <Animal icone="🦒" nome="Girafa" peso={1200} extincao={true} />
+        
+    </Exibicao>
 
-  </div>;
+    <Exibicao 
+    abertura={new Date("2024-11-06T13:00:00.000-03:00")}
+    fechamento={new Date("2024-11-06T17:00-03:00")}
+    cercado="B1"
+    >
+        <Animal icone="🦜" nome="Papagaio" peso={0.120} extincao={true} />
+    </Exibicao>
+  
+  </div>
+  );  
 } 
 
 export default App;
--- src/Exibicao.tsx
+++ src/Exibicao.tsx
@@ -1,17 +1,20 @@
+import { ReactNode } from "react";
 import "./Exibicao.css";
 
 interface ExibicaoProps {
     abertura: Date;
     fechamento: Date;
     cercado: string;
+    children?: ReactNode;
 }
 
-export default function Exibicao({abertura:inicio, fechamento:fim, cercado:local}:ExibicaoProps){
+export default function Exibicao({abertura:inicio, fechamento:fim, cercado:local, children:animais}:ExibicaoProps){
     return (
     <div className="exibicao">
         <div className="inicio">Abertura: {inicio.toLocaleString()}</div>
         <div className="fim">Fechamento: {fim.toLocaleString()}</div>
         <div className="local">Cercado: {local}</div>
+        <div className="animais">{animais}</div>
     </div>
     );
 }
