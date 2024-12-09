0ad6f8c5cd76c4aef4ad200dc124c5f1d0d6eb84
Luiza Caldeira <luiza.caldeira@estudante.ufjf.br>
2024-12-09 19:25:18 -0300

    Extrai vetores de componentes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import Animal from './Animal';
 import Exibicao from './Exibicao';
 import './App.css';
+import { ReactNode } from 'react';
 
 function App() {
+  const exA1:ReactNode[] = [
+    <Animal icone="🦁" nome="Leão" peso={-1} extincao/>,
+    <Animal icone="🦩" nome="Flamingo" peso={12} extincao />,
+  ];
+  const exB2:ReactNode[] = [
+    <Animal icone="🦒" nome="Girafa" peso={1200} extincao />,
+    <Animal icone="🦜" nome="Papagaio" peso={0.120} extincao />,
+  ];
   return (
   <div className='app'>
     <Exibicao 
@@ -10,17 +19,14 @@ function App() {
     fechamento={new Date("2024-11-06T12:00-03:00")}
     cercado="A1"
     >
-        <Animal icone="🦁" nome="Leão" peso={-1} extincao={true} />
-        <Animal icone="🦒" nome="Girafa" peso={1200} extincao={true} />
-        
+     {exA1}   
     </Exibicao>
 
     <Exibicao 
     abertura={new Date("2024-11-06T13:00:00.000-03:00")}
     fechamento={new Date("2024-11-06T17:00-03:00")}
     cercado="B1"
-    >
-        <Animal icone="🦜" nome="Papagaio" peso={0.120} extincao={true} />
+    > {exB2}
     </Exibicao>
   
   </div>
